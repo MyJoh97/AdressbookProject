ce2837d2575048f5bf0d5aabff9118fb88887ec9
My <my.j97@protonmail.com>
2023-12-30 17:28:59 +0100

    Tests

---
+++ Adressbook.Tests/PersonInfoService_Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adressbook.Tests
+{
+    public class PersonInfoService_Tests
+    {
+        [Fact]
+        public void AddToListShould_AddOnePersonInfoToPersonInfoList_ThenReturnTrue()
+        {
+
+        }
+    }
+}
